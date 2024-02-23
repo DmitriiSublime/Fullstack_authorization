4e417bbab1d3c744fb1c58b283b95ee7785f2caa
Dmitrii <115217175+DmitriiSublime@users.noreply.github.com>
2024-02-23 13:18:24 +0300

    Добавлена валидация

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import {Layout} from "../../components/layout";
+import {Card, Form, Row, Space, Typography} from "antd";
+import {CustomInput} from "../../components/custom-input";
+import {PasswordInput} from "../../components/password-input/input";
+import {CustomButton} from "../../components/custom-button";
+import { Link } from 'react-router-dom';
+import {Paths} from "../../paths";
 
 export const Login = () => {
     return (
         <Layout>
-            Login page
+            <Row align="middle" justify='center'>
+                <Card title='Войти' style={{width: "30rem"}}>
+                    <Form onFinish={() => null}>
+                        <CustomInput
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                        />
+                        <PasswordInput
+                            name="password"
+                            placeholder="Пароль"
+                        />
+                        <CustomButton  type="primary" htmlType="submit"  loading={false}>
+                            Войти
+                        </CustomButton>
+                    </Form>
+                    <Space direction="vertical" size="large">
+                        <Typography.Text>
+                            Нет аккаунта? <Link to={Paths.register}>Зарегистрироваться</Link>
+                        </Typography.Text>
+                    </Space>
+                </Card>
+            </Row>
         </Layout>
     );
 };
