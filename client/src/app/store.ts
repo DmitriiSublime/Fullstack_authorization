27dffe44fd2dee4b88c66b78e612345f84bad635
DmitriiSublime <godamongmen@bk.ru>
2024-03-08 14:51:46 +0300

    Исправлены ошибки

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,16 +1,20 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {configureStore, ThunkAction, Action} from '@reduxjs/toolkit';
+import {api} from "./services/api";
+import auth from "../features/authSlice";
+import {listenerMiddleware} from "../middleware/auth";
 
 export const store = configureStore({
-  reducer: {
+    reducer: {
+        [api.reducerPath]: api.reducer,
+        auth,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware).prepend(listenerMiddleware.middleware)
 
-  },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,
+    RootState,
+    unknown,
+    Action<string>>;
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import {Layout} from "../../components/layout";
-import {Card, Form, Row, Space, Typography} from "antd";
-import {CustomInput} from "../../components/custom-input";
-import {PasswordInput} from "../../components/password-input/input";
-import {CustomButton} from "../../components/custom-button";
-import { Link } from 'react-router-dom';
-import {Paths} from "../../paths";
+import { Layout } from "../../components/layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { CustomInput } from "../../components/custom-input";
+import { PasswordInput } from "../../components/password-input/input";
+import { CustomButton } from "../../components/custom-button";
+import { Link, useNavigate } from 'react-router-dom';
+import { Paths } from "../../paths";
+import { useLoginMutation, UserData } from "../../app/services/auth";
+import { isErrorWithMessage } from "../../utils/is-error-with-message";
+import { ErrorMessage } from "../../components/error-message";
 
 export const Login = () => {
+    const navigate = useNavigate();
+    const [loginUser, loginUserResult] = useLoginMutation();
+    const [error, setError] = React.useState('');
+
+    const login = async (data: UserData) => {
+        try {
+            await loginUser(data).unwrap();
+            navigate('/');
+        } catch (err) {
+            const maybeError = isErrorWithMessage(err);
+
+            if (maybeError) {
+                setError(err.data.message);
+            } else {
+                setError('Unknown error');
+            }
+        }
+    };
+
     return (
         <Layout>
             <Row align="middle" justify='center'>
-                <Card title='Войти' style={{width: "30rem"}}>
-                    <Form onFinish={() => null}>
+                <Card title='Войти' style={{ width: "30rem" }}>
+                    <Form onFinish={login}>
                         <CustomInput
                             type="email"
                             name="email"
@@ -22,14 +44,15 @@ export const Login = () => {
                             name="password"
                             placeholder="Пароль"
                         />
-                        <CustomButton  type="primary" htmlType="submit"  loading={false}>
-                            Войти
+                        <CustomButton type="primary" htmlType="submit" loading={false}>
+                            Войти
                         </CustomButton>
                     </Form>
                     <Space direction="vertical" size="large">
                         <Typography.Text>
                             Нет аккаунта? <Link to={Paths.register}>Зарегистрироваться</Link>
                         </Typography.Text>
+                        <ErrorMessage message={error} />
                     </Space>
                 </Card>
             </Row>
